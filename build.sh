d83aa107f46f095ff26a857dfabb86301a183874
undefinednpc <sasarehulka@gmail.com>
2021-12-21 11:32:54 +0100

    fixed the build script

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
 #/bin/sh
 
-gcc -o sway-gest main.c `pkg-config --cflags --libs libinput libudev`
-mv sway-gest bin/
+if [ -d "bin/" ]; then
+	gcc -o sway-gest main.c `pkg-config --cflags --libs libinput libudev`
+	mv sway-gest bin/
+	echo "Binary was generated to bin/"
+else
+	gcc -o sway-gest main.c `pkg-config --cflags --libs libinput libudev`
+	mkdir bin
+	echo "Created bin folder"
+	mv sway-gest bin/
+	echo "Binary was generated to bin/"
+fi
