d0d98dd528b15ad441faf15b7842c1a57698aa9f
undefinednpc <sasarehulka@gmail.com>
2021-12-23 13:58:25 +0100

    Working on detecting the gesture and according direction.

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
-#include <stddef.h>
+#include <assert.h>
 #include <sys/socket.h>
 
 #include <json-c/json_object.h>
 
 #include <libinput.h>
 #include <libudev.h>
+
+typedef enum { RIGHT, LEFT } Direction;
 
 static int open_restricted(const char *path, int flags, void *user_data)
 {
@@ -26,9 +28,24 @@ static const struct libinput_interface interface = {
 	.close_restricted = close_restricted,
 };
 
+Direction get_gesture_direction(struct libinput_event_gesture *event) 
+{
+	return 0;
+}
+
+static void sway_workspace_left()
+{
+	assert(!"Not Implemented");
+}
+
+static void sway_workspace_right()
+{
+	assert(!"Not Implemented");	
+}
+
 static void print_event_type(struct libinput_event *event)
 {
-	const char *type = NULL;
+	const char *type = 0;
 	switch(libinput_event_get_type(event)) {
 			case LIBINPUT_EVENT_DEVICE_ADDED:
 				type = "DEVICE ADDED";
@@ -41,17 +58,26 @@ int main()
 {
 	struct libinput *li;
 	struct libinput_event *event;
+	struct libinput_event_gesture *gesture;
 	struct udev *udev;
 
 	udev = udev_new();
 
-	li = libinput_udev_create_context(&interface, NULL, udev);
+	li = libinput_udev_create_context(&interface, 0, udev);
 	libinput_udev_assign_seat(li, "seat0");
 	libinput_dispatch(li);
 
-	while ((event = libinput_get_event(li)) != NULL) {
+	while ((event = libinput_get_event(li)) != 0) {
 
-		print_event_type(event);		
+		print_event_type(event);
+		gesture = libinput_event_get_gesture_event(event);
+		switch(get_gesture_direction(gesture)) {
+			case LEFT:
+				sway_workspace_left();
+				break;
+			case RIGHT:
+				sway_workspace_right();
+		}
 
 		libinput_event_destroy(event);
 		libinput_dispatch(li);
